a88d4ff8eeef9ae550b6204dd78090e27069d156
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-11 18:03:41 -0400

    Updated index.js.

--- index.js
+++ index.js
@@ -10,13 +10,15 @@
 // )
 
 
-const title = React.createElement(
-  "h1",
-  {id: "title", className: "header"},
-  "Hello World"
-)
 
-ReactDOM.render(
-  title,
-  document.getElementById("react-container")
-)
+//
+// const title = React.createElement(
+//   "h1",
+//   {id: "title", className: "header"},
+//   "Hello World"
+// )
+//
+// ReactDOM.render(
+//   title,
+//   document.getElementById("react-container")
+// )
