2b199e1f0051275a1c9fc1e714b56724bcb8cc8a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-11 17:15:04 -0400

    Updated index.js.

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const {render} = ReactDOM;
 render (
   <h1 id = "title"
       className = "header"
-      style = {{backgroundColor: "orange", }}
+      style = {{backgroundColor: "orange", color: }}
 )
