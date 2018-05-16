9ea226ec06088a3090dd78aa2ccd6baa3482d017
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-MBP.home>
2018-05-16 02:46:42 -0400

    Updated webpack.config.js.

--- oreiley-learn_react/webpack.config.js
+++ oreiley-learn_react/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
         filename: "bundle.js"
         },
     module: {
-rules: [ {
+        rules: [ {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
                 loader: ['babel-loader'],
