89b47d91123ba9fef02250941549649639623904
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-MBP.home>
2018-05-16 02:56:58 -0400

    Updated webpack.config.js.

--- oreiley-learn_react/webpack.config.js
+++ oreiley-learn_react/webpack.config.js
@@ -1,18 +1,23 @@
+var webpack = require("webpack");
 module.exports = {
-   entry: "./src/index.js",
+    entry: "./src/index.js",
     output: {
-        path: "./dist/assets/",
-        filename: "bundle.js"
-        },
+        path: "dist/assets",
+        filename: "bundle.js",
+        sourceMapFilename: 'bundle.map'
+    },
+    devtool: '#source-map',
     module: {
-        rules: [ {
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            loader: ['babel-loader'],
-            query: {
-                presets: ['env', 'stage-0', 'react']
-            }
-          }
-         ]
-       }
-      }
+rules: [ {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: ['babel-loader'],
+                query: {
+                    presets: ['env', 'stage-0', 'react']
+                }
+} ]
+}, plugins: [
+new webpack.optimize.UglifyJsPlugin({ sourceMap: true,
+warnings: false,
+mangle: true })
+] }
