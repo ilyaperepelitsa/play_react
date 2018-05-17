0c7078c8161885d0f07785920211d43afcfc471e
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-17 15:20:24 -0400

    Updated app.js.

--- udemy react complete webdev/indecision-app/src/app.js
+++ udemy react complete webdev/indecision-app/src/app.js
@@ -3,7 +3,9 @@
 console.log("App.js is running")
 
 var template = <div>
-      <h1>This is JSX. Does this change?</h1></div>
+      <h1>This is JSX. Does this change?</h1>
+
+      </div>
 var appRoot = document.getElementById("app");
 
 
