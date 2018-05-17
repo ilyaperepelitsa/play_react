0802bafd8ccc9778cbebcdf110df9ccf313fe83a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-17 14:37:26 -0400

    Updated app.js.

--- udemy react complete webdev/indecision-app/public/scripts/app.js
+++ udemy react complete webdev/indecision-app/public/scripts/app.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// live-server public
+
+console.log("App.js is running");
+
+var template = React.createElement(
+  "p",
+  null,
+  "This is JSX. Doe"
+);
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
---
+++ udemy react complete webdev/indecision-app/src/app.js
@@ -0,0 +1,9 @@
+// live-server public
+
+console.log("App.js is running")
+
+var template = <p>This is JSX. Doe</p>
+var appRoot = document.getElementById("app");
+
+
+ReactDOM.render(template, appRoot)
