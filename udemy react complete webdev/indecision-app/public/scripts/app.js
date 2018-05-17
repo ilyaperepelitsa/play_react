651827627965dcae335142d720e499c5c7f38328
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-17 16:25:19 -0400

    Updated app.js.

--- udemy react complete webdev/indecision-app/public/scripts/app.js
+++ udemy react complete webdev/indecision-app/public/scripts/app.js
@@ -4,21 +4,45 @@
 
 console.log("App.js is running");
 
-var template = React.createElement(
+// var template =
+//     <div>
+//       <h1>This is JSX. Does this change?</h1>
+//       <p> Some paragraph</p>
+//       <ol>
+//         <li>Pew</li>
+//         <li>PEWPEWPEW</li>
+//       </ol>
+//     </div>
+var userName = "Ilya";
+var userAge = 29;
+var userLocation = "New York, NY";
+
+var user = {
+  name: "Ilya Perepelitsa",
+  age: 29
+
+};
+var template2 = React.createElement(
   "div",
   null,
   React.createElement(
     "h1",
     null,
-    "This is JSX. Does this change?"
+    userName
   ),
   React.createElement(
     "p",
     null,
-    " Some paragraph"
+    "Age: ",
+    userAge
   ),
-  "          "
+  React.createElement(
+    "p",
+    null,
+    "Location: ",
+    userLocation
+  )
 );
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- udemy react complete webdev/indecision-app/src/app.js
+++ udemy react complete webdev/indecision-app/src/app.js
@@ -2,11 +2,31 @@
 
 console.log("App.js is running")
 
-var template =
+// var template =
+//     <div>
+//       <h1>This is JSX. Does this change?</h1>
+//       <p> Some paragraph</p>
+//       <ol>
+//         <li>Pew</li>
+//         <li>PEWPEWPEW</li>
+//       </ol>
+//     </div>
+var userName = "Ilya"
+var userAge = 29
+var userLocation = "New York, NY"
+
+var user = {
+  name: "Ilya Perepelitsa",
+  age: 29,
+  
+}
+var template2 =
     <div>
-      <h1>This is JSX. Does this change?</h1>
-      <p> Some paragraph</p>          </div>
+      <h1>{userName}</h1>
+      <p>Age: {userAge}</p>
+      <p>Location: {userLocation}</p>
+    </div>
 var appRoot = document.getElementById("app");
 
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template2, appRoot)
