d1c14be9205eccb81bb4979b4c6f6878b2f53bf0
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-18 05:08:46 -0400

    Updated app.js.

---
+++ udemy react complete webdev/indecision-app/public/scripts/app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var appRoot = document.getElementById("app");
+
+var app = {
+  title: "Indecision App",
+  subtitle: "Put thingy thingy thing",
+  options: ["One", "Two"]
+};
+
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    c;
+  }
+};
+
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "form",
+    { onSubmit: onFormSubmit },
+    React.createElement("input", { type: "text", name: "option" }),
+    React.createElement(
+      "button",
+      null,
+      "Add option"
+    )
+  )
+);
+
+ReactDOM.render(template, appRoot);
