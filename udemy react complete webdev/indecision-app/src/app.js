ff798f70a7cb85fb22e492ae8f04a31f93462ca0
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-18 01:18:11 -0400

    Updated app.js.

--- udemy react complete webdev/indecision-app/src/app.js
+++ udemy react complete webdev/indecision-app/src/app.js
@@ -1,6 +1,6 @@
 // live-server public
 // babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
-// babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+// babel src/playground/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 
 console.log("App.js is running")
 
