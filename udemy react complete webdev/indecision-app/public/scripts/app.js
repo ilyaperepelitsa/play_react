ee329a3a5e32d68b71a7ffc54776e2d3cdfb757f
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-18 01:20:38 -0400

    Updated let-const.js.

---
+++ udemy react complete webdev/indecision-app/public/scripts/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// var nameVar = "Andrew";
+// var nameVar = "Mike";
+// console.log("nameVar", nameVar)
+var nameLet = "Jen";
+// let nameLet = "Hanne";
+console.log("nameLet", nameLet);
