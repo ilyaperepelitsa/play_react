d04ba812b3853f9c815ccf228d5510dbd93ec34a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-18 03:07:46 -0400

    Updated arrow-func2.js.

--- udemy react complete webdev/indecision-app/src/playground/arrow-func2.js
+++ udemy react complete webdev/indecision-app/src/playground/arrow-func2.js
@@ -6,10 +6,10 @@ const add = function(a, b){
 console.log(add(2, 4, 500))
 const user = {
   name: "Andrew",
-  cities: ["Philadelphia", "New York", "Dublin"]
+  cities: ["Philadelphia", "New York", "Dublin"],
   printPlacesLived: function(){
     console.log(this.name);
     console.log(this.cities);
-    
+
   }
 }
