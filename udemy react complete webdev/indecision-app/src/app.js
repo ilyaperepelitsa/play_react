4d5bcd76b49a92cc82ef4c1f022cde9f6aca31fa
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-18 05:02:40 -0400

    Updated app.js.

--- udemy react complete webdev/indecision-app/src/app.js
+++ udemy react complete webdev/indecision-app/src/app.js
@@ -1,42 +1,4 @@
-// live-server public
-// babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
-// babel src/playground/let-const.js --out-file=public/scripts/app.js --presets=env,react --watch
 
-console.log("App.js is running")
-
-const template =
-    <div>
-      <h1>This is JSX. Does this change?</h1>
-      <p> Some paragraph</p>
-      <ol>
-        <li>Pew</li>
-        <li>PEWPEWPEW</li>
-      </ol>
-    </div>
-
-const userName = "Ilya"
-const userAge = 29
-const userLocation = "New York, NY"
-
-const user = {
-  name: "Ilya Perepelitsa",
-  age: 29,
-  location: "New York, NY"
-}
-
-function getLocation(location){
-  if (location){
-    return <p>Location: {location}</p>;
-  }
-}
-
-
-const template2 =
-    <div>
-      <h1>{user.name ? user.name : "Anonymous"}</h1>
-      {(user.age && user.age >= 18) && <p>Age (grown-up): {user.age}</p>}
-      {getLocation(user.location)}
-    </div>
 const appRoot = document.getElementById("app");
 
 
