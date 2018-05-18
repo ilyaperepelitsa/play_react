145d858d099cba9bd76c0ede162ddb96cdb653a5
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-18 00:17:53 -0400

    Updated app.js.

--- udemy react complete webdev/indecision-app/src/app.js
+++ udemy react complete webdev/indecision-app/src/app.js
@@ -40,7 +40,7 @@ var appRoot = document.getElementById("app");
 
 var post = {
   title: "This is a tutorial nonsense",
-  subtitle: "I wouldn't really look into it. It's pretty long",
+  subtitle: "I wouldn",
   // subtitle: "I wouldn't really look into it. It's pretty long",
   some_num: 30
 }
