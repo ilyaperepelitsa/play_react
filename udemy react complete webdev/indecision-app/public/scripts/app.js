021a1f91fa62a98e77f986aa8d64ddafb2568e20
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-18 03:26:53 -0400

    Updated arrow-func2.js.

--- udemy react complete webdev/indecision-app/public/scripts/app.js
+++ udemy react complete webdev/indecision-app/public/scripts/app.js
@@ -1,24 +1,19 @@
+// const add = function(a, b){
+//   console.log(arguments)
+//   return a + b;
+// }
+//
+// console.log(add(2, 4, 500))
+// const user = {
+//   name: "Andrew",
+//   cities: ["Philadelphia", "New York", "Dublin"],
+//   printPlacesLived(){
+//     return this.cities.map((city) => city + " pewpew");
+//     // this.cities.forEach((city)=>{
+//     //   console.log(this.name + " has lived in " + city);
+//     // })
+//   }
+// }
+//
+// console.log(user.printPlacesLived())
 "use strict";
-
-var add = function add(a, b) {
-  console.log(arguments);
-  return a + b;
-};
-
-console.log(add(2, 4, 500));
-var user = {
-  name: "Andrew",
-  cities: ["Philadelphia", "New York", "Dublin"],
-  printPlacesLived: function printPlacesLived() {
-    return this.cities.map(function (city) {
-      return city + " pewpew";
-    });
-
-    return pew;
-    // this.cities.forEach((city)=>{
-    //   console.log(this.name + " has lived in " + city);
-    // })
-  }
-};
-
-console.log(user.printPlacesLived());
--- udemy react complete webdev/indecision-app/src/playground/arrow-func2.js
+++ udemy react complete webdev/indecision-app/src/playground/arrow-func2.js
@@ -1,20 +1,18 @@
-const add = function(a, b){
-  console.log(arguments)
-  return a + b;
-}
-
-console.log(add(2, 4, 500))
-const user = {
-  name: "Andrew",
-  cities: ["Philadelphia", "New York", "Dublin"],
-  printPlacesLived(){
-    return this.cities.map((city) => city + " pewpew");
-
-    return pew;
-    // this.cities.forEach((city)=>{
-    //   console.log(this.name + " has lived in " + city);
-    // })
-  }
-}
-
-console.log(user.printPlacesLived())
+// const add = function(a, b){
+//   console.log(arguments)
+//   return a + b;
+// }
+//
+// console.log(add(2, 4, 500))
+// const user = {
+//   name: "Andrew",
+//   cities: ["Philadelphia", "New York", "Dublin"],
+//   printPlacesLived(){
+//     return this.cities.map((city) => city + " pewpew");
+//     // this.cities.forEach((city)=>{
+//     //   console.log(this.name + " has lived in " + city);
+//     // })
+//   }
+// }
+//
+// console.log(user.printPlacesLived())
