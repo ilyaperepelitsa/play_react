1fb5113d0a813b5cae2f393d1828777c426a121a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-31 21:53:23 -0400

    Updated app.js.

--- udemy react complete webdev ** IN PROGRESS/indecision-app/src/app.js
+++ udemy react complete webdev ** IN PROGRESS/indecision-app/src/app.js
@@ -1,3 +1,5 @@
+
+
 // live-server public
 // babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 // babel src/playground/let-const.js --out-file=public/scripts/app.js --presets=env,react --watch
