cc48891574b836bc96689daa843afec7770d3637
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-31 22:02:10 -0400

    Updated app.js.

--- udemy react complete webdev ** IN PROGRESS/indecision-app/src/app.js
+++ udemy react complete webdev ** IN PROGRESS/indecision-app/src/app.js
@@ -1,6 +1,6 @@
 yarn init
 yarn add babel-preset-react
-yarn add babel-preset-react
+yarn add babel-preset-env
 
 // live-server public
 // babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
