726d80e61ee69d6e1ee51cf0f155ae4a4a2527f4
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-06-04 14:45:32 -0400

    Updated conditional_rendering.js.

---
+++ udemy react complete webdev ** IN PROGRESS/indecision-app/src/playground/conditional_rendering.js
@@ -0,0 +1,30 @@
+// yarn init
+// yarn add babel-preset-react
+// yarn add babel-preset-env
+
+// live-server public
+// babel src/app.js --out-file=public/app.js --presets=env,react --watch
+
+// babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+// babel src/playground/let-const.js --out-file=public/scripts/app.js --presets=env,react --watch
+
+
+const appRoot = document.getElementById("app");
+
+var user = {
+  age:23,
+  name: "Peter",
+  message: "Pewpew"
+}
+
+var template = (
+  <div>
+    // good for when you want two options
+    <h1>{user.name ? user.name: "Anonymous"}</h1>
+    // good for when you want one thing ifit exists or nothing
+    {user.name && <h1>{user.name}</h1>}
+    {(user.age && user.age >= 18) && <h1>{user.age}</h1>}
+  </div>
+)
+
+ReactDOM.render(template, appRoot)
