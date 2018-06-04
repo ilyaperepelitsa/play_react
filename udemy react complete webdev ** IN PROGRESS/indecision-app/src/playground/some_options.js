2976ed42db512d23c283ca69af6220c6f0504758
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-06-04 05:52:29 -0400

    Updated some_options.js.

---
+++ udemy react complete webdev ** IN PROGRESS/indecision-app/src/playground/some_options.js
@@ -0,0 +1,41 @@
+// yarn init
+// yarn add babel-preset-react
+// yarn add babel-preset-env
+
+// live-server public
+// babel src/app.js --out-file=public/app.js --presets=env,react --watch
+
+// babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+// babel src/playground/let-const.js --out-file=public/scripts/app.js --presets=env,react --watch
+
+
+const appRoot = document.getElementById("app");
+
+const app = {
+  title: "Indecision App",
+  subtitle: "Put thingy thingy thing",
+  options: ["One", "Two"]
+}
+
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+  e.target.elements.option.value = "";
+
+  if (option){
+    app.options.push(option);
+  }
+}
+
+const template = (
+  <div>
+    <p>{app.options.length}</p>
+    <form onSubmit = {onFormSubmit}>
+      <input type="text" name="option"/>
+      <button>Add option</button>
+    </form>
+  </div>
+)
+
+ReactDOM.render(template, appRoot)
