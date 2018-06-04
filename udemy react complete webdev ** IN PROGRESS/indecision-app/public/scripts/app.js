a1a1003737e9fb31867f756ef29edc57b7854848
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-06-04 14:29:41 -0400

    Updated app.js.

---
+++ udemy react complete webdev ** IN PROGRESS/indecision-app/public/scripts/app.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// yarn init
+// yarn add babel-preset-react
+// yarn add babel-preset-env
+
+// live-server public
+// babel src/app.js --out-file=public/app.js --presets=env,react --watch
+
+// babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+// babel src/playground/let-const.js --out-file=public/scripts/app.js --presets=env,react --watch
+
+
+var appRoot = document.getElementById("app");
+
+var user = {
+  age: 23,
+  name: "Peter",
+  message: "Pewpew"
+};
+
+var template = React.createElement(
+  "div",
+  null,
+  "// good for when you want two options",
+  React.createElement(
+    "h1",
+    null,
+    user.name ? user.name : "Anonymous"
+  ),
+  "// good for when you want one thing ifit exists or nothing",
+  user.name && React.createElement(
+    "h1",
+    null,
+    user.name
+  )
+);
+
+ReactDOM.render(template, appRoot);
