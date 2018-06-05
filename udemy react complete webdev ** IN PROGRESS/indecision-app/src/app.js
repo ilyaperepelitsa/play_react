3d1201581b4154c533e82b39ff37b6e81c90a5a0
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-06-05 12:51:50 -0400

    Updated app.js.

--- udemy react complete webdev ** IN PROGRESS/indecision-app/src/app.js
+++ udemy react complete webdev ** IN PROGRESS/indecision-app/src/app.js
@@ -5,7 +5,7 @@ const render = () =>
     <div>
       <h1>Visibility toggle</h1>
       <button onClick = >
-        {visibility ? }
+        {visibility ? "Hide deta"}
       </button>
     </div>
   }
