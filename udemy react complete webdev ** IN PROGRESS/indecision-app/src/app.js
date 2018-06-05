a91efb1a85ac2c1ae0e86d9f49619012048e7a24
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-06-05 16:08:22 -0400

    Updated app.js.

--- udemy react complete webdev ** IN PROGRESS/indecision-app/src/app.js
+++ udemy react complete webdev ** IN PROGRESS/indecision-app/src/app.js
@@ -2,7 +2,7 @@
 class Person {
   constructor(name = "adsa", age = 123){
     this.name = name;
-    this.age = 
+    this.age = age;
   }
   getGreeting(){
     return `Hi blah blah blah ${this.name}`;
