bde11347f27cc264f1ad3f3367cbd5530a65182d
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-06-05 12:49:59 -0400

    Updated app.js.

--- udemy react complete webdev ** IN PROGRESS/indecision-app/src/app.js
+++ udemy react complete webdev ** IN PROGRESS/indecision-app/src/app.js
@@ -7,24 +7,3 @@
 
 // babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 // babel src/playground/let-const.js --out-file=public/scripts/app.js --presets=env,react --watch
-
-
-const appRoot = document.getElementById("app");
-
-var user = {
-  age:23,
-  name: "Peter",
-  message: "Pewpew"
-}
-
-var template = (
-  <div>
-    // good for when you want two options
-    <h1>{user.name ? user.name: "Anonymous"}</h1>
-    // good for when you want one thing ifit exists or nothing
-    {user.name && <h1>{user.name}</h1>}
-    {(user.age && user.age >= 18) && <h1>{user.age}</h1>}
-  </div>
-)
-
-ReactDOM.render(template, appRoot)
