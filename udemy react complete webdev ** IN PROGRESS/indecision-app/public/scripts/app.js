b9fc6b60d2e64392a8338c219110e6bc0ac493a6
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-06-05 12:51:14 -0400

    Updated app.js.

---
+++ udemy react complete webdev ** IN PROGRESS/indecision-app/public/scripts/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var visibility = false;
+
+var render = function render() {
+  React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      "Visibility toggle"
+    )
+  );
+};
--- udemy react complete webdev ** IN PROGRESS/indecision-app/src/app.js
+++ udemy react complete webdev ** IN PROGRESS/indecision-app/src/app.js
@@ -3,6 +3,6 @@ let visibility = false;
 const render = () =>
   {
     <div>
-      <
+      <h1>Visibility toggle</h1>
     </div>
   }
